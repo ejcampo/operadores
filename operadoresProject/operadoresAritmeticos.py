8c3c5eff9fa27b1dec7a4f466da68bb5efec64aa
EDUAR JULIAN CAMPO CALDONO <1002918483@fup.edu>
2024-09-26 16:49:23 -0500

    Se crea el proyecto con los ejemplos de los operadores

---
+++ operadoresProject/operadoresAritmeticos.py
@@ -0,0 +1,42 @@
+#Ejemplo utilizando operadores aritmeticos
+numero1 = int(input("Ingrese el numero1: "))
+numero2 = int(input("Ingrese el numero2: "))
+
+suma = numero1 + numero2
+print(f"la suma de {numero1} y {numero2} es: {suma}")
+
+#resta
+numero3 = int(input("Ingrese el numero3: "))
+numero4 = int(input("Ingrese el numero4: "))
+
+resta = numero3 - numero4
+print("la resta es: ",str(resta))
+
+#multiplicacion
+numero5 = int(input("Ingrese el numero5: "))
+numero6 = int(input("Ingrese el numero6: "))
+
+multiplicaion = numero5 * numero6
+print("la multiplicacion es: ",str(multiplicaion))
+
+#division
+numero7 = int(input("Ingrese el numero7: "))
+numero8 = int(input("Ingrese el numero8: "))
+
+division = numero7 / numero8
+print("la division es: ",str(division))
+
+#modulo
+numero9 = int(input("Ingrese el numero9: "))
+numero10 = int(input("Ingrese el numero10: "))
+
+modulo = numero1 % numero2
+print("la suma es: ",str(modulo))
+
+#divisionEntera
+numero1 = int(input("Ingrese el numero1: "))
+numero2 = int(input("Ingrese el numero2: "))
+
+suma = numero1 + numero2
+print("la suma es: ",str(suma))
+
